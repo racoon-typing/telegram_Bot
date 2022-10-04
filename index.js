c2985b08717b34ef4ae7b411dc704dd3b7970a7e
Daniil <eo18622@yandex.ru>
2022-10-04 09:20:57 +0300

    crhbgn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// const require = ;
+const TelegramApi = require('node-telegram-bot-api');
 
 const token = '5767402335:AAGrPlY-lDSlUTkwAcL8bM-lmhzAzDX2_GI';
 
+const bot = new TelegramApi(token, {polling: true});
+
+bot.on('message', msg => {
+    console.log(msg);
+});
