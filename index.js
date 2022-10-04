194a41d0523619f565945f3f03bcda9708904545
Daniil <eo18622@yandex.ru>
2022-10-04 15:12:35 +0300

    функция

--- index.js
+++ index.js
@@ -17,11 +17,26 @@ const gameOptions = {
     })
 };
 
+const againOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Играть еще раз', callback_data: '/again'}],
+        ]
+    })
+};
+
 bot.setMyCommands([
     {command: '/start', description: 'Начальное приветствие'},
     {command: '/info', description: 'Получить информацию о пользователе '},
     {command: '/game', description: 'Игра угадай цифру'},
 ]);
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее отгадать`);
+    const randomNumber = Math.floor(Math.random() * 10); 
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
+}
 
 const start = () => {
     bot.on('message', async msg => {
@@ -38,10 +53,7 @@ const start = () => {
         }
 
         if (text === '/game') {
-            await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее отгадать`);
-            const randomNumber = Math.floor(Math.random() * 10); 
-            chats[chatId] = randomNumber;
-            return bot.sendMessage(chatId, 'Отгадывай', gameOptions);
+            return startGame(chatId);
         }
     
         return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)')
@@ -50,13 +62,16 @@ const start = () => {
     bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
+        if (data === '/again') {
+            return startGame(chatId);
+        }
+
         if (data === chats[chatId]) {
-            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`);
+            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions);
         } else {
-            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`);
+            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions);
         }
     });
 }
-
 
 start();
