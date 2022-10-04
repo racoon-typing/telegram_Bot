c4663cc7210abe974ffccb42d61131f485da0f72
Daniil <eo18622@yandex.ru>
2022-10-04 09:46:15 +0300

    cjj,otybt

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ const token = '5767402335:AAGrPlY-lDSlUTkwAcL8bM-lmhzAzDX2_GI';
 const bot = new TelegramApi(token, {polling: true});
 
 bot.on('message', msg => {
-    console.log(msg);
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    if (text === '/start') {
+        bot.sendMessage(chatId, `https://tlgrm.eu/_/stickers/7e8/aa6/7e8aa67b-ad91-4d61-8f62-301bde115989/256/1.webp`);
+        bot.sendMessage(chatId, `Добро пожаловать в телеграм бот: Даниила С`);
+    }
+
+    if (text === '/info') {
+        bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`); 
+    }
+
+
+    bot.sendMessage(chatId, `Ты написал мне ${text}`);
 });
